a60098c6a8b9bec2fd989c37fc13a65a1595a24f
Pablo Fuentes Candel <pafuente@student.42malaga.com>
2025-01-26 13:22:02 +0100

    adding some functions that I will use in gnl.h and ft_strlen implemented in gnl.c

--- milestone1/get_next_line/get_next_line.h
+++ milestone1/get_next_line/get_next_line.h
@@ -6,7 +6,26 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:23:29 by pafuente          #+#    #+#             */
-/*   Updated: 2025/01/26 11:23:30 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/01/26 13:14:51 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
--- milestone1/get_next_line/get_next_line_utils.c
+++ milestone1/get_next_line/get_next_line_utils.c
@@ -6,7 +6,28 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:22:32 by pafuente          #+#    #+#             */
-/*   Updated: 2025/01/26 11:22:34 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/01/26 13:18:18 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int main ()
+{
+	char str[]="Hola Mundo";
+	int result = ft_strlen(str);
+	printf("LETRAS: %d", result);
+	return 0;
+}
