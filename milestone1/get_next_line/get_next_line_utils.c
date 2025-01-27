8e0da5285204b8750716396c93e769b58a703379
Pablo Fuentes Candel <pafuente@student.42malaga.com>
2025-01-27 13:38:00 +0100

    strjoin added

--- milestone1/get_next_line/get_next_line_utils.c
+++ milestone1/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:22:32 by pafuente          #+#    #+#             */
-/*   Updated: 2025/01/26 13:18:18 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/01/27 13:33:00 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,63 @@ size_t	ft_strlen(char *s)
 	return (i);
 }
 
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return (&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*joined_str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	joined_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!joined_str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		joined_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		joined_str[i + j] = s2[j];
+		j++;
+	}
+	joined_str[i + j] = '\0';
+	return (joined_str);
+}
+
+
 int main ()
 {
-	char str[]="Hola Mundo";
-	int result = ft_strlen(str);
-	printf("LETRAS: %d", result);
+	 char str1[] = "Hola, ";
+    char str2[]="mundo";
+
+	char *result = ft_strjoin(str1, str2);
+
+    // Imprimimos el resultado
+    if (result) {
+        printf("%s\n", result);  // Imprime "Hola, mundo"
+        free(result);  // Liberamos la memoria reservada
+    }
+
+    
 	return 0;
 }
